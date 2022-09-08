0fb0352345d025d2cda899ae4916d4c515df3725
Supreeth-Shetty <shetty.supreeth95@gmail.com>
2022-09-08 16:36:27 +0530

    Folder Restructured

--- Personal_Finance/Personal_Finance/asgi.py
+++ Personal_Finance/Personal_Finance/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Personal_Accountant project.
+ASGI config for Personal_Finance project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Personal_Accountant.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Personal_Finance.settings")
 
 application = get_asgi_application()
--- Personal_Finance/User_Authentication/apps.py
+++ Personal_Finance/User_Authentication/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UserAuthConfig(AppConfig):
+class UserAuthenticationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "User_Auth"
+    name = "User_Authentication"
