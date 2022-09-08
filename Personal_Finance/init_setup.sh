1e1ed3989f353695c9e2c42f78f48308e96fa950
Supreeth-Shetty <shetty.supreeth95@gmail.com>
2022-09-08 16:56:44 +0530

    gitignore modified

--- Personal_Finance/init_setup.sh
+++ Personal_Finance/init_setup.sh
@@ -1,6 +1,6 @@
 echo [$(date)]: "START"
 echo [$(date)]: "creating environment"
-conda create --prefix ./env python=3.7 -y
+conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activate environment"
 source activate ./env
 echo [$(date)]: "installing requirements"
@@ -9,7 +9,7 @@ echo [$(date)]: "export conda environment"
 conda env export > conda.yaml
 echo [$(date)]: "add env to gitignore"
 echo "env/" > .gitignore
-# echo [$(date)]: "installing setup.py"
-# pip install -e .
+echo [$(date)]: "installing setup.py"
+pip install -e .
 # pip freeze
 echo [$(date)]: "initiall setup is done!!"
