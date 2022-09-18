eef7dcb13fd85efdb91628e45a35b271aff3342a
Supreeth-Shetty <shetty.supreeth95@gmail.com>
2022-09-18 19:11:59 +0530

    static dir changed

--- Personal_Finance/settings.py
+++ Personal_Finance/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "Personal_Finance_React/build/static")
+    os.path.join(BASE_DIR, "static")
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
